de47b5fd8206353d1107597483587a996de2da0b
Ayush <ajadnaev@mail.ru>
2022-09-08 17:42:44 +0300

    log&reg makeup

--- clientApp/components/Login/Login.jsx
+++ clientApp/components/Login/Login.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, TextInput, View, Button,
+} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { colors } from '../../constants';
 
 function Login() {
   return (
-    <View>
-      <Text>Login</Text>
+    <View
+      style={{
+			  backgroundColor: colors.GOLD,
+			  height: '100%',
+			  justifyContent: 'center',
+			  alignItems: 'center',
+      }}
+    >
+      <View style={{ width: 300 }}>
+        <TextInput
+          style={stylesCreated.input}
+          placeholder="Enter email"
+        />
+        <TextInput
+          style={stylesCreated.input}
+          placeholder="Enter password"
+          secureTextEntry
+        />
+        <TouchableOpacity style={stylesCreated.input}>
+          <Text style={{ textAlign: 'center' }}>Войти</Text>
+        </TouchableOpacity>
+        {/* <Button
+          title="login"
+          color="#fff"
+          style
+        /> */}
+      </View>
     </View>
   );
 }
+
+const stylesCreated = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+});
 
 export default Login;
