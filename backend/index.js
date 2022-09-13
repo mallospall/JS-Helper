8393bfde734cbb47726f76c79a9bc891ae6771e2
Sabre-hil <abdeevhizir95@gmail.com>
2022-09-13 17:42:00 +0300

    fix parse Habr

--- backend/index.js
+++ backend/index.js
@@ -12,17 +12,19 @@ const parse = async () => {
     const pageNumber = href('a.tm-pagination__page').eq(-1).text();
     console.log(pageNumber);
     const arr = [];
-    for (let i = 1; i < pageNumber; i++) {
+    for (let i = 1; i < 40; i++) {
       const selector = await getHTML(
         `https://habr.com/ru/flows/develop/news/page${i}/`,
       );
       selector('.tm-article-snippet').each((i, element) => {
-        const title = selector(element).find('a.tm-article-snippet__title-link');
+        const link = `https://habr.com${selector(element).find('a.tm-article-snippet__title-link').attr('href')}`;
+        const title = selector(element).find('a.tm-article-snippet__title-link').text();
         arr.push(title);
+        console.log(link);
         console.log(title);
       });
     }
-    res.json(title);
+    // res.json(title);
   } catch (error) {
     console.log('iui!', error);
   }
